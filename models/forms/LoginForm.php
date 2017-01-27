1a5b6d83687ebb0bfa0b5540d61715614af7ed50
Marek Niemczyk <marekn@nordplus.pl>
2017-01-27 06:22:52 +0100

    fixed label for username if email is used as login

--- models/forms/LoginForm.php
+++ models/forms/LoginForm.php
@@ -32,7 +32,7 @@ class LoginForm extends Model
 	public function attributeLabels()
 	{
 		return [
-			'username'   => UserManagementModule::t('front', 'Username'),
+			'username'        => Yii::$app->getModule('user-management')->useEmailAsLogin ? 'E-mail' : UserManagementModule::t('front', 'Login'),
 			'password'   => UserManagementModule::t('front', 'Password'),
 			'rememberMe' => UserManagementModule::t('front', 'Remember me'),
 		];
